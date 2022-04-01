0829b9793bb54291339dcd2ab153be17a9355a0b
jere0015 <43599281+jere0015@users.noreply.github.com>
2022-04-01 14:04:54 +0200

    Updated API and Blazor version

--- MineSweeper.API/Startup.cs
+++ MineSweeper.API/Startup.cs
@@ -26,6 +26,7 @@ namespace MineSweeper.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ namespace MineSweeper.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
