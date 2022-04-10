ef940654e466b2566310935adfb83a09386e6a7b
Rerup <Jaco222r@edu.ucl.dk>
2022-04-10 17:44:34 +0200

    Connected Blazor to API

---
+++ MineSweeper.Blazor/Services/IScoreService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Blazor.Services
+{
+    public interface IScoreService
+    {
+
+        Task<IEnumerable<Score>> GetScores();
+
+        Task<IEnumerable<Score>> GetScore(int id);
+
+        Task<IEnumerable<Score>> PostScore(Score score);
+
+    }
+}
--- MineSweeper.Blazor/Startup.cs
+++ MineSweeper.Blazor/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MineSweeper.Blazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,13 @@ namespace MineSweeper.Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddHttpClient<IScoreService, ScoreService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/");
+            });
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
