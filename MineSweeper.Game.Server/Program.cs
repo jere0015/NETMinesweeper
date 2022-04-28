f5c891551f0f667e54b9197d252a57201b4a8de8
chri06ea <chri06ea@edu.ucl.dk>
2022-04-28 18:53:39 +0200

    Update Program.cs

--- MineSweeper.Game.Server/Program.cs
+++ MineSweeper.Game.Server/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-builder.Services.AddSingleton<IGameFactory, GameFactory>();
+builder.Services.AddSingleton<IGameFactory, GameProxyFactory>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
