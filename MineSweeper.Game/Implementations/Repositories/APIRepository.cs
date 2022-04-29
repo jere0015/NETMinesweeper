be1790204675e1a6fab2463530c6805361afc1f3
chri06ea <chri06ea@edu.ucl.dk>
2022-04-29 13:08:46 +0200

    UI Update

---
+++ MineSweeper.Game/Implementations/Repositories/APIRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Game.Implementations.Repositories
+{
+    //internal class APIRepository<T> : IRepository<T> where T : class
+    //{
+    //    public APIRepository(HttpClient httpClient)
+    //    {
+    //        HttpClient = httpClient;
+    //    }
+
+    //    public HttpClient HttpClient { get; }
+
+    //    public async Task Create(T entity)
+    //    {
+    //        var json = JsonSerializer.Serialize(entity);
+
+    //        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+    //        await HttpClient.PostAsync("", content);
+
+    //        return;
+    //    }
+
+    //    public IEnumerable<T> Items()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
